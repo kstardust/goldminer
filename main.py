82dd58d420b36af710f5cb0de76987c3df6215c8
stardust <lql9705@gmail.com>
2022-12-31 10:25:34 +0800

    sp500 filter

--- main.py
+++ main.py
@@ -1,155 +1,131 @@
-import pandas as pd
-import pymongo
-import yfinance
-import requests
-import multiprocessing
-from multiprocessing.pool import ThreadPool
+import pandas
+
+import sp500
+import copy
 import datetime
 import logging
-import setting
-from dataclasses import dataclass
-from YFTickerProxyWrapper import TickerProxyWrapper
 
 
-@dataclass
-class Stock:
-    Name: str
-    Sector: str
-    Symbol: str
+class GoldFilterBase:
+    def __init__(self, Miner):
+        self.miner = Miner
+        self.input = None
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def GetOutput(self):
+        # get filter output
+        pass
+
+    def Feed(self, data):
+        self.input = data
+        # input data
+        pass
 
 
-class GoldMinerSP500Stats:
+class GoldMiner:
+    def __init__(self, sp500):
+        self.filters = []
+        self.sp500 = sp500
+        self.intermediate_data = {}
+        self.logger = logging.getLogger("GoldMiner")
 
-    COL_NAME = 'sp500_stats'
-    UselessTickerStatFields = ['upgradeDowngradeHistory']
+    def SetIntermediateData(self, name, value):
+        self.intermediate_data[name] = value
 
-    def __init__(self):
-        self.mongoCli = pymongo.MongoClient(setting.MONGO_URI)
-        self.database = self.mongoCli.get_database(setting.DB_NAME)
-        self.logger = logging.getLogger('GoldMinerSP500Stats')
-        self.sp500Json = None
-        self.tickers = None
+    def GetIntermediateData(self, name):
+        return self.intermediate_data.get(name)
 
-    def GetDBCol(self):
-        return self.database[self.COL_NAME]
+    def AddFilter(self, filter: GoldFilterBase):
+        self.filters.append(filter)
 
-    def GetSP500(self):
-        Resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', proxies={"https": setting.YF_PROXY})
+    def Run(self):
+        data = copy.deepcopy(self.sp500)
+        for filter in self.filters:
+            self.logger.info(f"run filter {filter.__class__.__name__}")
+            filter.Feed(data)
+            data = filter.GetOutput()
 
-        if not Resp.ok:
-            self.logger.error(f"cannot get datapackage, ret: {Resp.status_code}")
-            return None
+        return data
 
-        table = pd.read_html(Resp.text)
-        df = table[0]
 
-        sp500 = []
-        for index, row in df.iterrows():
-            sp500.append({
-                'Symbol': row['Symbol'],
-                'Name': row['Security'],
-                'Sector': row['GICS Sector']
-            })
-        self.sp500Json = sp500
-        return self.sp500Json
+class PEFilter(GoldFilterBase):
+    TENYEAR_TREASURY = 3.751 / 100
 
-    def GetLogger(self):
-        return self.logger
+    def GetMaxPE(self):
+        return 1 / self.TENYEAR_TREASURY * 0.8
 
-    def GetTickers(self):
-        if self.tickers:
-            return self.tickers
+    def GetOutput(self):
+        passedTicker = {}
+        maxPE = self.GetMaxPE()
+        peSum = 0
+        tickerCount = 0
+        for ticker, data in self.input.items():
+            summaryDetail = data.get('summaryDetail')
 
-        sp500 = self.GetSP500()
+            if not summaryDetail:
+                self.logger.error(f"cannot fetch summary of {ticker}")
+                continue
 
-        pool = ThreadPool(processes=multiprocessing.cpu_count() * 2)
-        tickers = {}
+            pe = summaryDetail.get('trailingPE')
+            if pe is None:
+                self.logger.error(f"cannot fetch meaningful PE of {ticker}")
+                continue
 
-        def fetch_task(stock):
-            retries = 3
+            divRate = summaryDetail.get('dividendRate')
+            if divRate is None:
+                continue
 
-            name = stock['Name']
-            symbol = stock['Symbol']
-            sector = stock['Sector']
-            self.logger.info(f'fetching stats for ${symbol}, Company Name: {name}, Sector: {sector}')
-            while True:
-                if retries == 0:
-                    self.logger.error(f"max retries reached, exit.")
-                    exit(-1)
-                try:
-                    t = TickerProxyWrapper(yfinance.Ticker(symbol), setting.YF_PROXY)
-                    result = t.stats()
-                    if not result:
-                        retries -= 1
-                        self.logger.error(f"invalid stat, retrie: {retries} \n{e}")
-                        continue
+            price = data['price']['regularMarketPrice']
 
-                    for key in self.UselessTickerStatFields:
-                        result.pop(key, None)
-                    return {symbol: result}
-                except:
-                    import traceback
-                    e = traceback.format_exc()
-                    self.logger.error(f"error occurred during fetching stat, retrie: {retries} \n{e}")
-                    retries -= 1
+            keyStat = data['defaultKeyStatistics']
+            price2Book = keyStat.get('priceToBook')
+            if not price2Book:
+                self.logger.error(f"invalid price to book {ticker}")
+                continue
 
-        for stock in sp500:
-            pool.apply_async(fetch_task, [stock], callback=lambda data: tickers.update(data))
+            tickerCount += 1
+            peSum += pe
+            if pe <= maxPE:
+                self.logger.info(f'{ticker} passed, PE {pe}, divRate {divRate / price}, P2B {price2Book}')
+                passedTicker[ticker] = data
 
-        pool.close()
-        pool.join()
+        self.logger.info(f"Total {tickerCount}, the average PE is {peSum / tickerCount}")
+        return passedTicker
 
-        self.tickers = tickers
-        return self.tickers
 
-    def UseDataofDate(self, date):
+def PrintData(data, sp500Name):
+    ticker2name = {t['Symbol']: t['Name'] for t in sp500Name}
 
-        if isinstance(date, datetime.datetime):
-            date = date.strftime("%Y-%m-%d")
+    df = pandas.DataFrame(columns=['Sector', 'Name', 'Ticker', 'Price', 'Price2Book', 'PE', 'DivRate'])
 
-        result = self.GetDBCol().find_one({'date': date})
-        if result:
-            self.tickers = result['tickers']
-            self.sp500Json = result['sp500']
-            return True
+    n = 0
+    for ticker, tickerData in data.items():
+        price = tickerData['price']['regularMarketPrice']
+        price2Book = tickerData['defaultKeyStatistics']['priceToBook']
+        pe = tickerData['summaryDetail']['trailingPE']
+        dividendRate = tickerData['summaryDetail']['dividendRate'] / price
+        sector = tickerData['summaryProfile']['sector']
+        name = ticker2name.get(ticker, ticker)
 
-        return False
+        df.loc[n] = [sector, name, ticker, price, price2Book, pe, dividendRate]
+        n += 1
 
-    def UpdateDatabase(self):
-        '''
-        update the lastest sp500 stats data from internet and save to database
-        '''
+    df.to_csv("sp500-2022-12-14.csv", index=False)
 
-        self.GetSP500()
-        self.GetTickers()
-        self.Save()
 
-    def Save(self):
-        if self.sp500Json is None or self.tickers is None:
-            self.logger.error("cannot save, invalid data")
-            return
+def main():
+    SP500 = sp500.GoldMinerSP500Stats()
+    if not SP500.UseDataofDate("2022-12-14"):
+        logging.getLogger("main").error("error no sp500 data")
+        return
 
-        now = datetime.datetime.now()
-        date = now.strftime("%Y-%m-%d")
+    Miner = GoldMiner(SP500.GetTickers())
 
-        self.GetDBCol().update_one(
-            {
-                'date': date
-            },
-            {
-                '$set': {
-                    'sp500': self.sp500Json,
-                    'tickers': self.tickers,
-                    'save_time': now,
-                    'date': date,
-                }
-            },
-            upsert=True
-        )
-        self.logger.info(f"data of {date} has been written to database.")
+    Miner.AddFilter(PEFilter(Miner))
+
+    data = Miner.Run()
+    PrintData(data, SP500.GetSP500())
 
 
 if __name__ == '__main__':
-    GDSP500 = GoldMinerSP500Stats()
-    #GDSP500.UseDataofDate(datetime.datetime.now())
-
+    main()
